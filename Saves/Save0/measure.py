77dded684e6c3a46e405ee4e61ce94344d79e701
ivan.blinov <ivan.blinov@vk.team>
2026-07-05 02:25:30 +0300

    carrots, sunflower, measure

---
+++ Saves/Save0/measure.py
@@ -0,0 +1,19 @@
+from __builtins__ import *
+
+# noinspection PyTypeChecker
+def measure(action):
+    before = {}
+    for item in Items:
+        before[item] = num_items(item)
+
+    t0 = get_time()
+    action()
+    t1 = get_time()
+
+    result = {'time': t1 - t0}
+    for item in Items:
+        diff = num_items(item) - before[item]
+        if diff:
+            result[item] = diff
+
+    return result
--- Saves/Save0/sunflower.py
+++ Saves/Save0/sunflower.py
@@ -3,15 +3,15 @@ import move
 import water
 
 def cycle(x0=0, y0=0, s=min(8, get_world_size())):
-    change_hat(Hats.Carrot_Hat)
+    change_hat(Hats.Sunflower_Hat)
     for x in range(s):
         for y in range(s):
             move.to(x + x0, y + y0)
 
             if get_ground_type() != Grounds.Soil:
                 till()
-            if get_entity_type() != Entities.Carrot or can_harvest():
+            if get_entity_type() != Entities.Sunflower or can_harvest():
                 harvest()
-                plant(Entities.Carrot)
+                plant(Entities.Sunflower)
             else:
                 water.apply()
