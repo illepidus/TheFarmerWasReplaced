7d0c085b941d9df387cc4991db7cc6aacdc13f9f
ivan.blinov <ivan.blinov@vk.team>
2026-07-05 19:57:40 +0300

    substance naive

--- Saves/Save0/substance.py
+++ Saves/Save0/substance.py
@@ -1,15 +1,26 @@
 from __builtins__ import *
+import move
 
 
-def cycle():
+def cycle(x=0, y=0, target=64):
+    clear()
+    change_hat(Hats.Carrot_Hat)
+    move.to(x, y)
+    i = 0
     if get_ground_type() != Grounds.Soil:
         till()
     if get_entity_type() != Entities.Carrot:
         plant(Entities.Carrot)
 
     while True:
+        if i >= target:
+            break
+
         if can_harvest():
             harvest()
             plant(Entities.Carrot)
+            i += 1
         else:
             use_item(Items.Fertilizer)
+
+    clear()
