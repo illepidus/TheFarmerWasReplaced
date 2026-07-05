40f50af60a6e36d45f47c615a740985d1dab1923
ivan.blinov <ivan.blinov@vk.team>
2026-07-05 13:02:37 +0300

    substance naive

--- Saves/Save0/substance.py
+++ Saves/Save0/substance.py
@@ -1,18 +1,15 @@
 from __builtins__ import *
-import move
-
-change_hat(Hats.Carrot_Hat)
-move.to(0, 0)
 
 
-if get_ground_type() != Grounds.Soil:
-    till()
-if get_entity_type() != Entities.Carrot:
-    plant(Entities.Carrot)
-
-while True:
-    if can_harvest():
-        harvest()
+def cycle():
+    if get_ground_type() != Grounds.Soil:
+        till()
+    if get_entity_type() != Entities.Carrot:
         plant(Entities.Carrot)
-    else:
-        use_item(Items.Fertilizer)
+
+    while True:
+        if can_harvest():
+            harvest()
+            plant(Entities.Carrot)
+        else:
+            use_item(Items.Fertilizer)
