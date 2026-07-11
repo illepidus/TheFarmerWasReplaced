283f5fe6509d3b6c2cb34b47a9b1ef877df28f88
ivan.blinov <ivan.blinov@vk.team>
2026-07-11 19:55:23 +0300

    single_pumpkin step

--- Saves/Save0/pumpkin_single.py
+++ Saves/Save0/pumpkin_single.py
@@ -1,63 +1,33 @@
 from __builtins__ import *
-import move
-import water
+from _movement import fly
 
 
-def _init(size):
-    field = []
-    v = 1
-    for _ in range(size):
-        row = []
-        for _ in range(size):
-            row.append(v)
-            v += 1
-        field.append(row)
-    return field, v
+def _init(p0, size):
+    queue = list()
+    for i in range(size):
+        for j in range(size):
+            queue.append((p0[0] + i, p0[1] + j))
+    return queue
 
-
-def _find_min(field):
-    min_x = 0
-    min_y = 0
-    min_v = None
-
-    for y in range(len(field)):
-        for x in range(len(field[y])):
-            v = field[y][x]
-            if v != 0:
-                if min_v == None or v < min_v:
-                    min_v = v
-                    min_x = x
-                    min_y = y
-
-    return min_x, min_y, min_v
-
-
-def cycle(x0=0, y0=0, s=min(6, get_world_size())):
+def cycle(x0=0, y0=0, s=6):
     change_hat(Hats.Pumpkin_Hat)
+    queue = _init((x0, y0), s)
 
-    field, next_v = _init(s)
-
-    while True:
-        x, y, v = _find_min(field)
-        if v == None:
-            break
-
-        move.to(x + x0, y + y0)
+    while len(queue) > 0:
+        task = queue.pop(0)
+        fly(task)
 
         if get_ground_type() != Grounds.Soil:  # no pumpkins can grow here
             till()
 
         if get_entity_type() != Entities.Pumpkin:  # dead pumpkin / not a pumpkin
             harvest()
-            water.apply()
             plant(Entities.Pumpkin)
-            field[y][x] = next_v
-            next_v += 1
-        elif can_harvest():  # nice! this pumpkin is nice and grown lets forget about it
-            field[y][x] = 0
-        else:  # still not grown pumpkin, let's move to next one
-            water.apply()
-            field[y][x] = next_v
-            next_v += 1
+            queue.append(task)
+        elif not can_harvest():  # still not grown pumpkin, let's move to next one
+            queue.append(task)
 
     harvest()
+
+
+cycle(0, 0, 32)
